7cee21547e956a26d7de2741e99de205752aeeca
Henri Borodavkin <hborodavkin@broadsoft.com>
2016-02-16 09:47:45 +0200

    BTUC-10541: qt-5.5-BTUC-10541
    BTUC-10541: FIxed patch paths for menu open crash.
    
    BTUC-10541: Added patch for menu open crash.

--- src/controls/qquickmenupopupwindow.cpp
+++ src/controls/qquickmenupopupwindow.cpp
@@ -86,6 +86,7 @@ void QQuickMenuPopupWindow::setParentWindow(QWindow *effectiveParentWindow, QQui
         setTransientParent(effectiveParentWindow);
     m_logicalParentWindow = parentWindow;
     if (parentWindow) {
+        connect(parentWindow, SIGNAL(destroyed()), this, SLOT(dismissPopup()));
         if (QQuickMenuPopupWindow *pw = qobject_cast<QQuickMenuPopupWindow *>(parentWindow)) {
             connect(pw, SIGNAL(popupDismissed()), this, SLOT(dismissPopup()));
             connect(pw, SIGNAL(willBeDeletedLater()), this, SLOT(setToBeDeletedLater()));
