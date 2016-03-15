be27683a5b2ae26743fa38dc25796025337ce1cd
Kimmo Karvinen <kkarvinen@broadsoft.com>
2016-03-15 09:12:29 +0200

    BTUC-10848: Fixed the Alt mnemonic showing under extension menu
    Change-Id: Ia3f085358dc5d29b05156914ee2938cf93b95274

--- src/controls/qquickmenuitem.cpp
+++ src/controls/qquickmenuitem.cpp
@@ -100,6 +100,7 @@ void QQuickMenuBase::setParentMenu(QQuickMenu *parentMenu)
         m_parentMenu->platformMenu()->removeMenuItem(m_platformItem);
 
     m_parentMenu = parentMenu;
+    emit parentChanged();
 }
 
 QQuickMenuItemContainer *QQuickMenuBase::container() const
